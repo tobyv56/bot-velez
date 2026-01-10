f5a50cd10cb9a60d1c4c5ea5e733806a555c85e2
toby_velez <115357174+tobyv56@users.noreply.github.com>
2026-01-10 11:46:21 -0300

    Refactor main.py for improved readability and structure

--- main.py
+++ main.py
@@ -207,9 +207,9 @@ async def responder_whatsapp(Body: str = Form(...)):
         media_type="application/xml"
     )
 
-    try:
-    db_pool = psycopg2.pool.SimpleConnectionPool(
-        1, 20,
+try:
+        db_pool = psycopg2.pool.SimpleConnectionPool(
+                    1, 20,
         os.environ.get('DATABASE_URL'),
         sslmode='require'
     )
@@ -245,6 +245,7 @@ def ejecutar_query(query, params=None, es_consulta=True):
 def get_connection():
     # Esto intentará conectar de nuevo si la conexión se perdió
     return psycopg2.connect(os.environ.get('DATABASE_URL'), sslmode='require')
+
 
 
 
