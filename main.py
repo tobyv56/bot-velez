d70477570785eb6515812d04ef81879b0ca74a85
toby_velez <115357174+tobyv56@users.noreply.github.com>
2026-01-11 19:18:20 -0300

    Refactor WhatsApp responder and database handling

--- main.py
+++ main.py
@@ -1,60 +1,61 @@
-from fastapi import FastAPI, HTTPException, status, Form, Response
+from fastapi import FastAPI, HTTPException,status,Form,Response
 from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from typing import Optional
 from twilio.twiml.messaging_response import MessagingResponse
 import os
-from datetime import datetime
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 app = FastAPI()
 
 class Product(BaseModel):
-    nombre_producto: Optional[str] = None
+    nombre_producto: Optional[str] = None #solamente dice que el atributo es opcional para modificarlo
     precio : Optional[float] = None
     fecha_vencimiento : Optional[str] = None
     marca : Optional[str] = None
     stock : Optional[int] = None
 
-@app.get("/")
-async def home():
-    return {"mensaje": "El bot está encendido y escuchando"}
-    
+conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
+
+cursor = conn.cursor()
+
 @app.post("/webhook")
 async def responder_whatsapp(Body: str = Form(...)):
     
     mensaje = Body.strip()
-    if not mensaje: 
-        return Response(content=str(MessagingResponse()), media_type="application/xml")
+    if not mensaje: return Response(content=str(MessagingResponse()), media_type="application/xml")
     
-    partes = mensaje.split()
+    partes = mensaje.split() #rompe la cadena
     comando = partes[0].lower()
+
     resp_twilio = MessagingResponse()
     respuesta = ""
 
-   
-    try:
-        conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
+    if comando == "!producto":
+        
+        consulta_limpia = " ".join(partes[1:]).strip()
 
-        if comando == "!producto":
-            consulta_limpia = " ".join(partes[1:]).strip()
-            if not consulta_limpia:
-                respuesta = "❌ ¿Qué buscás? Ej: !producto quilmes lata"
-            else:
-                palabras = consulta_limpia.replace(",", " ").split()
-                query_base = "SELECT nombre_producto, stock, precio, fecha_vencimiento, marca FROM producto WHERE "
+        if not consulta_limpia:
+            respuesta = "❌ ¿Qué buscás? Ej: !producto quilmes lata"
+        else:
+            try:
+                conn.rollback() 
+                
+                palabras = consulta_limpia.replace(",", " ").split() # formateo la cadena
+                
+                query_base = "SELECT nombre_producto, stock, precio, fecha_vencimiento, marca FROM producto WHERE " # arma la query
                 condiciones = []
                 parametros = []
 
                 for p in palabras:
-                    condiciones.append("(nombre_producto ILIKE %s OR marca ILIKE %s)")
-                    termino = f"%{p}%"
-                    parametros.extend([termino, termino])
+                    condiciones.append("(nombre_producto ILIKE %s OR marca ILIKE %s)") #implemento la condicion del where
+                    termino = f"%{p}%" #armo el termino del like 
+                    parametros.extend([termino, termino]) #"Desarma" la segunda lista y mete sus elementos uno por uno.
 
-                query_final = query_base + " AND ".join(condiciones) + " LIMIT 1"
+                query_final = query_base + " AND ".join(condiciones) + " LIMIT 1" #arma toda la query entera
+
                 cursor.execute(query_final, parametros)
                 producto = cursor.fetchone()
 
@@ -68,17 +69,28 @@ async def responder_whatsapp(Body: str = Form(...)):
                     )
                 else:
                     respuesta = f"❌ No encontré nada que tenga: *{consulta_limpia}*"
-
-        elif comando == "!productoc":
-            codigo_barra = " ".join(partes[1:]).strip()
             
-            if len(codigo_barra) < 1:
-                respuesta = "⚠️ Por favor, ingresá un código de barras."
-            else:
+            except Exception as e:
+                conn.rollback()
+                print(f"Errorazo: {e}") #devuelve un error textual de la bdd
+                respuesta = "⚠️ Hubo un fallo en la base de datos."
+    
+    elif comando == "!productoc":
+        
+        codigo_barra = " ".join(partes[1:]).strip()
+
+        conn.rollback()
+        
+        if len(codigo_barra) != 11 and not codigo_barra.isdigit():
+                respuesta = "codigo de barra incorrecto...."
+        else:
                 query_producto = """
                 SELECT nombre_producto, stock, precio, fecha_vencimiento, marca
-                FROM producto WHERE codigo = %s LIMIT 1
+                FROM producto
+                WHERE codigo_barra = %s
+                LIMIT 1
                 """
+            
                 cursor.execute(query_producto, (codigo_barra,))
                 producto = cursor.fetchone()
 
@@ -93,77 +105,135 @@ async def responder_whatsapp(Body: str = Form(...)):
                 else:
                     respuesta = "❌ Producto no encontrado"
 
-        
-        elif comando == "!nuevo":
-        
-            texto_datos = mensaje.replace("!nuevo", "").strip()
-            lista_datos = [p.strip() for p in texto_datos.split(",") if p.strip()]
+    elif comando == "!nuevo":
+        texto_datos = mensaje.replace("!nuevo", "").strip()
+        lista_datos = [p.strip() for p in texto_datos.split(",") if p.strip()]
 
-            if len(lista_datos) < 6:
-                respuesta = "❌ Formato incorrecto."
-            else:
-                try:
-                    nombre_p = lista_datos[0]
-                    precio = float(lista_datos[1])
-                    fecha_vencimiento = datetime.strptime(lista_datos[2], "%Y-%m-%d").date()
-                    marca = lista_datos[3]
-                    stock = int(lista_datos[4])
-                    codigo_barra = lista_datos[5]
-
-                    query_insercion = """INSERT INTO producto (nombre_producto,precio,fecha_vencimiento,marca,stock,codigo) VALUES
-                                         (%s,  %s,  %s, %s, %s, %s);"""
-                    cursor.execute(query_insercion,(nombre_p,precio,fecha_vencimiento,marca,stock,codigo_barra))
-                    conn.commit()
-                    respuesta = f"el producto {nombre_p} fue creado con exito!"
-                    
-                except Exception as e:
-                    respuesta = f"❌ Error: {e}"
-        elif comando == "!actualizar":
-            datos = mensaje.replace("!actualizar", "").strip()
-            lista_datos = [p.strip() for p in datos.split(",") if p.strip()]
-
-            if len(lista_datos) != 4:
-                respuesta = "⚠️ Error: Usá !actualizar nombre,marca,columna,valor"
-            else:
-                nombre_busq, marca_busq, atributo, valor = lista_datos
-                
-                cursor.execute(
-                    "SELECT id FROM producto WHERE nombre_producto ILIKE %s AND marca ILIKE %s LIMIT 1",
-                    (f"%{nombre_busq}%", f"%{marca_busq}%")
-                )
-                resultado = cursor.fetchone()
-
-                if not resultado:
-                    respuesta = "❌ No encontré el producto para actualizar."
-                else:
-                    id_prod = resultado["id"]
-                    query_upd = f"UPDATE producto SET {atributo} = %s WHERE id = %s"
-                    cursor.execute(query_upd, (valor, id_prod))
-                    conn.commit()
-                    respuesta = f"✅ Producto '{nombre_busq}' actualizado: {atributo} = {valor}."
-
-        elif comando == "!":
-            respuesta = (
-                "🤖 *Comandos Disponibles:*\n"
-                "• !producto [nombre]\n"
-                "• !productoc [codigo]\n"
-                "• !nuevo [nom,pre,fec,stk,mar,cod]\n"
-                "• !actualizar [nom,mar,campo,valor]"
-            )
+        if len(lista_datos) < 6:
+            respuesta = "❌ Formato incorrecto. Usá: !nuevo nombre,precio,fecha,stock,marca,codigo de barra"
         else:
-            respuesta = "❓ Comando no reconocido. Escribí *!* para ayuda."
+            try:
 
-    except Exception as e:
-        if 'conn' in locals(): conn.rollback()
-        print(f"❌ Error en BD: {e}")
-        respuesta = "⚠️ Error interno. Intentá de nuevo."
+                conn.rollback()
+
+                nombre_producto = lista_datos[0].strip()
+                precio = float(lista_datos[1].strip())
+                fecha_vencimiento = lista_datos[2].strip()
+                stock = int(lista_datos[3].strip())
+                marca = lista_datos[4].strip()
+                codigo_barra = lista_datos[5].strip()
+
+                insercion_producto = """
+                INSERT INTO producto
+                (nombre_producto, precio, fecha_vencimiento, stock, marca,codigo_barra)
+                VALUES (%s, %s, %s, %s, %s,%s)
+                """
+                cursor.execute(
+                    insercion_producto,
+                    (nombre_producto, precio, fecha_vencimiento, stock, marca, codigo_barra)
+                )
+                conn.commit()
+
+                respuesta = (
+                    "✅ Producto creado\n"
+                    f"Nombre: {nombre_producto}\n"
+                    f"Precio: {precio}\n"
+                    f"Vencimiento: {fecha_vencimiento}\n"
+                    f"Stock: {stock}\n"
+                    f"Marca: {marca}\n"
+                    f"codigo_barra: {codigo_barra}\n"
+                )
+
+            except Exception as e:
+                respuesta = f"❌ Error BD: {str(e)}"
     
-    finally:
-        if 'cursor' in locals(): cursor.close()
-        if 'conn' in locals(): conn.close()
+    elif comando == "!actualizar":
+        datos = mensaje.replace("!actualizar", "").strip()
+        lista_datos = [p.strip() for p in datos.split(",") if p.strip()]
+
+        if len(lista_datos) != 4:
+            respuesta = "⚠️ Error: Faltan datos."
+        else:
+            nombre_largo, marca_larga, atributo, valor = lista_datos
+
+            palabra_prod = nombre_largo.split()[0]
+
+            palabras_marca = marca_larga.split()
+
+            if len(palabras_marca) > 1 and palabras_marca[0].lower() in ["la", "el", "los", "las", "de"]:
+                palabra_marca = palabras_marca[1] 
+            else:
+                palabra_marca = palabras_marca[0] 
+
+            query = """
+                SELECT id, nombre_producto, marca 
+                FROM producto 
+                WHERE nombre_producto ILIKE %s 
+                AND marca ILIKE %s
+                LIMIT 1
+            """
+    
+            p_prod = f"%{palabra_prod}%"
+            p_marc = f"%{palabra_marca}%"
+
+            cursor.execute(query, (p_prod, p_marc))
+            resultado = cursor.fetchone()
+
+            if not resultado:
+                respuesta = f"❌ No encontré coincidencia para '{palabra_prod}' de '{palabra_marca}'"
+            else:
+                id_producto = resultado["id"]
+
+                query_update = f"UPDATE producto SET {atributo} = %s WHERE id = %s"
+                cursor.execute(query_update, (valor, id_producto))
+                conn.commit()
+                
+                respuesta = f"producto actualizado con exito!"
+    elif comando == "!":
+        respuesta = (f"comandos disponibles:\n"
+                    f"!producto: nombre_producto marca\n" 
+                    f"!productoc: codigo_barra\n"
+                    f"!nuevo: nombre_producto,precio,fecha_vencimiento,marca,stock,codigo_barra\n"
+                    f"!actualizar: nombre_producto,marca,atributo a modificar, por ej: nombre,stock,nuevo valor\n")
+    elif comando == "!vencimiento":
+         query_fecha_venc = """SELECT nombre_producto,fecha_vencimiento FROM producto
+                     WHERE 
+                     fecha_vencimiento BETWEEN CURRENT_DATE AND CURRENT_DATE + INTERVAL '62 days'
+                     AND 
+                     fecha_vencimiento >= CURRENT_DATE"""
+    cursor.execute(query_fecha_venc)
+    query_fecha = cursor.fetchall()
+
+    productos_por_vencer = []
+
+    for producto in query_fecha:
+        print(f"   -> Procesando: {producto['nombre_producto']}")
+        nombre_producto = producto['nombre_producto']
+        fecha_producto = producto['fecha_vencimiento']
+
+        texto = f"el producto {nombre_producto} vence el {fecha_producto}"
+
+        productos_por_vencer.append(texto)
+
+    cursor.close()
+    conn.close()
+
+    if len(productos_por_vencer) > 0:
+        print(f"3. Intentando enviar {len(productos_por_vencer)} productos a Twilio...")
+        respuesta = "reporte diario de los productos por vencer"
+        cuerpo = "\n".join(productos_por_vencer) 
+        respuesta_final = respuesta + cuerpo
+    
+    else:
+        respuesta = "❓ Comando no reconocido"
 
     resp_twilio.message(respuesta)
-    return Response(content=str(resp_twilio), media_type="application/xml")
+
+    return Response(
+        content=str(resp_twilio),
+        media_type="application/xml"
+    )
+
 
 
 
